6c4a4d14402db22cf4145cc1f77a9bb8cbe2acec
Half4376 <coretruthfleet@gmail.com>
2024-09-19 15:01:16 -0700

    Added word count function

--- main.py
+++ main.py
@@ -3,10 +3,21 @@ def main():
     text = get_book_text(book_path)
     print(text)
 
+    count = get_word_count(text)
+    print(count)
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def get_word_count(text):
+    word_count = 0
+    words = text.split()
+
+    for word in words:
+        word_count += 1    
+    
+    return word_count
 
 main()
