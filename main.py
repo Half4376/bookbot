b47e4c526d588c0d7491f6c45fe87c33caa632c5
Half4376 <coretruthfleet@gmail.com>
2024-09-19 16:33:28 -0700

    Final step in project, printed a report

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    #print(text)
 
     count = get_word_count(text)
-    print(count)
+    #print(count)
 
     char_count = count_characters(text)
-    print(char_count)
+    #print(char_count)
+
+    print("\n--- Begin report of books/frankenstein.txt ---")
+    print(f"{count} words found in the document\n")
+
+    sorted_char_count = sorted(char_count.items(), key=lambda item: item[1], reverse=True)
+
+    for char, count in sorted_char_count:
+        print(f"The {char} character was found {count} times")
+    
+    print("--- End report ---")
 
 
 def get_book_text(path):
