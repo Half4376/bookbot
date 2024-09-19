09a88453aec4d7a0f972f217ab0fb399f9bbb93b
Half4376 <coretruthfleet@gmail.com>
2024-09-19 15:39:17 -0700

    Added count_character function

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def main():
     count = get_word_count(text)
     print(count)
 
+    char_count = count_characters(text)
+    print(char_count)
+
 
 def get_book_text(path):
     with open(path) as f:
@@ -19,5 +22,17 @@ def get_word_count(text):
         word_count += 1    
     
     return word_count
+
+def count_characters(text):
+
+    character_count = {}
+    for char in text.lower():
+        if char.isalpha():
+            if char in character_count:
+                character_count[char] += 1
+            else:
+                character_count[char] = 1
+
+    return character_count
 
 main()
